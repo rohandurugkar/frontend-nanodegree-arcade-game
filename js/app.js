2d4882527e4abb340c262e38fd9b18d63de7f872
Rohan Durugkar <rohandurugkar@gmail.com>
2017-03-21 11:50:26 +0100

    added enemy speed and movement

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -8,6 +8,7 @@ var Enemy = function(x, y) {
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -16,6 +17,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x < 500){
+      this.x = this.x + (dt * 100) * this.speed;
+    } else {
+      this.x = -100;
+    }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -44,10 +51,18 @@ Player.prototype.render = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-allEnemies.push(new Enemy(0, 60));
-allEnemies.push(new Enemy(0, 145));
-allEnemies.push(new Enemy(0, 230));
-allEnemies.push(new Enemy(0, 315));
+
+//rows counted from the bottom
+//bug on fifth row
+allEnemies.push(new Enemy(0, 60, 4));
+//bugs on the third
+allEnemies.push(new Enemy(270, 145, 2.3));
+allEnemies.push(new Enemy(0, 145, 3));
+//bug on the second
+allEnemies.push(new Enemy(70, 230, 2));
+//bugs on the first row
+allEnemies.push(new Enemy(0, 315, 1));
+allEnemies.push(new Enemy(260, 315, 1.6));
 
 var player = new Player(200, 420);
 
@@ -60,6 +75,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
